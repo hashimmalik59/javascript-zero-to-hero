fcd949c3d236f479eb2f51c6ec2903ef3f017fed
hashimmalik59 <hm3630450@gmail.com>
2026-01-12 14:34:58 -0300

    linking JS file & variables and values

---
+++ 01-fundamentals-part-1/01-linking-js-file/app.js
@@ -0,0 +1,3 @@
+// Linking a JS file
+
+// Always link a JS file just before body closing tag
---
+++ 01-fundamentals-part-1/02-variables-values/app.js
@@ -0,0 +1,11 @@
+// Variables & Values
+
+// Variable => Box
+// Values => Item
+
+
+// name => Box
+// Hashim => Item
+
+let name = "Hashim";
+console.log(name);
