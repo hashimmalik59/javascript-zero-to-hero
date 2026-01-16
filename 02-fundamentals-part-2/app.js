3adc459eac5a8c320b2aa90a192920431256a443
hashimmalik59 <hm3630450@gmail.com>
2026-01-16 17:18:10 -0300

    Strict mode with some examples

---
+++ 02-fundamentals-part-2/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // 's' miss ho gaya (Typo)
+// JS yahan error nahi dega, bas ek naya variable bana dega.
+// Aap dhoondte reh jaoge ke License true kyun nahi hua.
+
+const interface = 123;
+const private = "name"
+console.log(private);
