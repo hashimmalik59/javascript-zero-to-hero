1f224f97920d67c01a9125f610fc70bcfa09a91d
hashimmalik59 <hm3630450@gmail.com>
2026-01-21 19:11:14 +0500

    Number guessing game live

---
+++ 04-javascript-in-the-browser-dom-and-events-projects/01-number-guessing-game/app.js
@@ -0,0 +1,57 @@
+//Accessing elements
+const container = document.querySelector(".container");
+const resetButton = document.querySelector(".reset");
+const correctNumber = document.querySelector(".correct");
+const input = document.querySelector("input");
+const checkButton = document.querySelector(".check");
+const message = document.querySelector(".message");
+const userScore = document.querySelector(".score");
+const userHighscore = document.querySelector(".highscore");
+let secretNumber = Math.floor(Math.random() * 20) + 1
+let score = 20;
+let highscore = 0;
+checkButton.addEventListener("click", () => {
+    if (+input.value === secretNumber) {
+        container.style.backgroundColor = "green";
+        message.textContent = "Correct Number!"
+        correctNumber.textContent = secretNumber;
+        if (score > highscore) {
+            highscore = score;
+            userHighscore.textContent = `Highscore: ${highscore}`;
+        }
+    } else {
+        score--;
+        userScore.textContent = `Score: ${score}`;
+        if (input.value === "") {
+            score = 20;
+            userScore.textContent = `Score: ${score}`;
+            message.textContent = "Invalid number...";
+            input.style.border = "4px solid red";
+            message.style.color = "red";
+        } else if (score < 1) {
+            userScore.textContent = "Game over...";
+            checkButton.style.opacity = ".5";
+            container.style.backgroundColor = "red";
+            message.textContent = "You lose...";
+        }
+        else if (input.value > secretNumber) {
+            message.textContent = "Too High";
+        } else {
+            message.textContent = "Too Low";
+        }
+    }
+})
+
+resetButton.addEventListener("click", () => {
+    container.style.backgroundColor = "#1f2937";
+    correctNumber.textContent = "?";
+    input.value = "";
+    message.textContent = "Start guessing..."
+    message.style.color = "#fff";
+    input.style.border = "2px solid white";
+    score = 20;
+    userScore.textContent = `Score: ${score}`;
+    checkButton.style.opacity = "1"
+    secretNumber = Math.floor(Math.random() * 20) + 1;
+    console.log(secretNumber);
+})
